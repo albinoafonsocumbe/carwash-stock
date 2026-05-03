63c0f5a603a75cc933c8d5897ece90a836ed1f4c
CarWash Stock <carwash@admin.com>
2026-05-03 14:54:03 +0200

    fix: corrigir staticfiles storage, adicionar paginas 404 e 500

--- carwash_stock/config/settings.py
+++ carwash_stock/config/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# WhiteNoise - usar versao simples para evitar erros com ficheiros em falta
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+else:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
