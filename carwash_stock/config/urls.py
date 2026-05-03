b71e339d0c2ed3c175a744d3548f320294293116
CarWash Stock <carwash@admin.com>
2026-05-03 13:18:13 +0200

    feat: landing page publica de apresentacao do produto

--- carwash_stock/config/urls.py
+++ carwash_stock/config/urls.py
@@ -3,9 +3,20 @@ URL configuration for carwash_stock project.
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+class LandingView(TemplateView):
+    template_name = 'landing.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            from django.shortcuts import redirect
+            return redirect('dashboard:index')
+        return super().dispatch(request, *args, **kwargs)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,5 +27,5 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('reports/', include('reports.urls')),
     path('lavagens/', include('lavagens.urls')),
-    path('', RedirectView.as_view(url='/accounts/registar/', permanent=False)),
+    path('', LandingView.as_view(), name='landing'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
